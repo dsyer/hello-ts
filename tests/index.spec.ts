7d97e927cf2a92d881a7fcaa7179fc99c650d130
Dave Syer <dsyer@vmware.com>
2022-08-23 13:43:44 +0100

    Add library

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,5 +1,15 @@
 import test from 'ava';
 
+import { sum, subtract } from '../src/index.js';
+
 test("testing", t => {
 	t.true(1 > 0);
+});
+
+test("add", t => {
+	t.is(sum(1,2), 3);
+});
+
+test("minus", t => {
+	t.is(subtract(3,2), 1);
 });
