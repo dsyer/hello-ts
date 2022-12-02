9ef565652e5b5fe9c33286ac843b2f09a4f57af6
Dave Syer <dsyer@vmware.com>
2022-12-02 13:54:55 +0000

    Use tsc not ts-node

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -3,13 +3,13 @@ import test from 'ava';
 import { sum, subtract } from '../src/index.js';
 
 test("testing", t => {
-	t.true(1 > 0);
+    t.true(1 > 0);
 });
 
 test("add", t => {
-	t.is(sum(1,2), 3);
+    t.is(sum(1, 2), 3);
 });
 
 test("minus", t => {
-	t.is(subtract(3,2), 1);
+    t.is(subtract(3, 2), 1);
 });
